8b8792111372c5d59710d8ab4ac66369848255fb
Turan-Alekberov <bethoven.92@mail.ru>
2024-05-01 09:59:17 +0400

    task

---
+++ js/style.js
@@ -0,0 +1,47 @@
+// let number = prompt("Eded daxil edin:");
+
+// if (number % 2 == 0) {
+//     alert(number ** 2);
+// } 
+// else if (number % 2 == 1) {
+//     alert(number ** 3);
+// }
+// else{
+//     alert("Duzgun eded daxil edin");
+// }
+
+
+// let a = prompt("Şeher adi dahil et");
+// if (a == "Baki") {
+//     alert("Çatdırılma var");
+
+// }
+// else {
+//    alert("Çatdırılma yoxdur");
+// }
+// let a = Number(prompt(" eded yaz"));
+// let b = Number(prompt(" eded yaz"));
+// let c = Number(prompt(" eded yaz"));
+// if (a % 2 == 0 && b % 2 == 0 && c % 2 == 0) {
+
+//     alert(a * b * c);
+// }
+// else if (a % 2 == 1 && b % 2 == 1 && c % 2 == 1) {
+
+//     alert(a + b + c);
+// }
+// else { alert("ededlerin fergi"(a - b - c)); }
+let a = Number(prompt(" eded yaz"));
+if (a >= 0) {
+    alert("eded musbetdir:");
+}
+else if (a < 0) {
+    alert("eded  menfidir");
+
+}
+else {
+    alert("eded  0");
+           
+}
+
+
